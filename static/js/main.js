94bb8b432445c28da984090f0cd3948f9af0d84b
alxndrpsclt <alxndr.psclt@gmail.com>
2020-08-08 17:18:57 +0200

    Adds gridster for chart grid

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+    var gridster;
+console.log("Coucou");
+
+        gridster = $(".gridster ul").gridster({
+            widget_base_dimensions: [100, 100],
+            widget_margins: [5, 5],
+            shift_widgets_up: false,
+            shift_larger_widgets_down: false,
+            collision: {
+                wait_for_mouseup: true
+            }
+        }).data('gridster');
+
