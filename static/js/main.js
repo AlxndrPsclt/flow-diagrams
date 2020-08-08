894dbcde9496926587f58178d1a734643b742aca
alxndrpsclt <alxndr.psclt@gmail.com>
2020-08-08 17:55:14 +0200

    Adds resize ability

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
+
 	var gridster;
 	console.log("Coucou");
 
 	gridster = $(".gridster ul").gridster({
-		widget_base_dimensions: [100, 100],
+		widget_base_dimensions: [200, 200],
 		widget_margins: [5, 5],
 		shift_widgets_up: false,
 		shift_larger_widgets_down: false,
 		collision: {
 			wait_for_mouseup: true
+		},
+		resize: {
+			enabled: true
 		}
 	}).data('gridster');
+
+
+	$('.js-resize-random').on('click', function () {
+		gridster.resize_widget(gridster.$widgets.eq(getRandomInt(0, 9)),
+			getRandomInt(1, 4), getRandomInt(1, 4))
+	});
 })
