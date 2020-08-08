668c2674301d85172759b9b104494a2eb5ab94f0
alxndrpsclt <alxndr.psclt@gmail.com>
2020-08-08 17:31:16 +0200

    Move js from index to static/js/main.js adding a wait fordocument to load

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
-    var gridster;
-console.log("Coucou");
+$(document).ready(function(){
+	var gridster;
+	console.log("Coucou");
 
-        gridster = $(".gridster ul").gridster({
-            widget_base_dimensions: [100, 100],
-            widget_margins: [5, 5],
-            shift_widgets_up: false,
-            shift_larger_widgets_down: false,
-            collision: {
-                wait_for_mouseup: true
-            }
-        }).data('gridster');
-
+	gridster = $(".gridster ul").gridster({
+		widget_base_dimensions: [100, 100],
+		widget_margins: [5, 5],
+		shift_widgets_up: false,
+		shift_larger_widgets_down: false,
+		collision: {
+			wait_for_mouseup: true
+		}
+	}).data('gridster');
+})
