38162cf1df71393bb9cf70f33468a0cbbe89e3a0
alxndrpsclt <alxndr.psclt@gmail.com>
2020-08-11 01:33:42 +0200

    Adds autoresizing of text in text input

--- static/js/main.js
+++ static/js/main.js
@@ -17,7 +17,9 @@ $(document).ready(function(){
 
 	gridster = $(".gridster ul").gridster({
 		widget_base_dimensions: [200, 200],
-		widget_margins: [5, 5],
+		widget_margins: [0, 0],
+		min_cols: 50,
+		min_rows: 50,
 		shift_widgets_up: false,
 		shift_larger_widgets_down: false,
 		collision: {
@@ -44,21 +46,71 @@ $(document).ready(function(){
 	connectAll();
 
 	document.addEventListener("click", (e) => {
-		const flyoutElements = document.getElementsByClassName("task-card");
-		const flyoutArray = Array.from(flyoutElements);
-		let targetElement = e.target; // clicked element
+		if (event.ctrlKey) {
+		  console.log("The CTRL key was pressed!");
 
-		do {
-			if (flyoutArray.includes(targetElement)) {
-				// This is a click inside. Do nothing, just return.
-				console.log("Clicked inside!");
-				return;
-			}
-				// Go up the DOM
-				targetElement = targetElement.parentNode;
-			} while (targetElement);
+			const flyoutElements = document.getElementsByClassName("task-card");
+			const flyoutArray = Array.from(flyoutElements);
+			let targetElement = e.target; // clicked element
 
-		// This is a click outside.
-		console.log("Clicked outside!");
+			do {
+				if (flyoutArray.includes(targetElement)) {
+					// This is a click inside. Do nothing, just return.
+					console.log("Clicked inside!");
+					return;
+				}
+					// Go up the DOM
+					targetElement = targetElement.parentNode;
+				} while (targetElement);
+
+			// This is a click outside.
+			console.log("Clicked outside!");
+			console.log(e);
+			col=Math.floor(e.pageX / 200)+1;
+			row=Math.floor(e.pageY / 200)+1;
+			console.log(e.pageX);
+			console.log(col);
+			console.log(e.pageY);
+			console.log(row);
+			gridster.add_widget('<li class="task-card" id="card03">YEAAAH</li>"', 1, 1, col, row);
+
+		} else {
+		  console.log("The CTRL key was NOT pressed!");
+		}
+
 	});
+
+
+	function handleKeyUp(event) {
+	  const BASE_FONT_SIZE = 8
+	  const MIN_FONT_SIZE = 3
+
+	  const input = event.target
+
+	  const inputLength = input.value.length
+	  const inputFontSize = input.style.fontSize
+
+	  let newFontSize = BASE_FONT_SIZE
+
+	  if (inputLength > BASE_FONT_SIZE) {
+		    newFontSize = BASE_FONT_SIZE - Math.round(inputLength / BASE_FONT_SIZE)
+		  }
+
+	  input.style.fontSize =
+	    newFontSize < MIN_FONT_SIZE ? `${MIN_FONT_SIZE}vh` : `${newFontSize}vh`
+	}
+
+	function handleBlur(event) {
+	  const input = event.target
+	  
+	  if (!input.innerText.trim().length) {
+		    input.innerHTML = ""
+		  }
+	}
+
+	const input = document.querySelector('#content')
+
+	input.addEventListener('keyup', handleKeyUp)
+	input.addEventListener('blur', handleBlur)
+
 })
