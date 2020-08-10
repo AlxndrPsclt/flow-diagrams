9d802632bee92b4cd79cc9568f289f3336526ef5
alxndrpsclt <alxndr.psclt@gmail.com>
2020-08-10 22:57:38 +0200

    Adds click detection inside and outside of the elements

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,11 @@ $(document).ready(function(){
 
 	console.log("Coucou");
 
+//$( "#outer" ).click(function(e) {
+//  console.log(e);
+//});
+
+
 	gridster = $(".gridster ul").gridster({
 		widget_base_dimensions: [200, 200],
 		widget_margins: [5, 5],
@@ -32,9 +37,28 @@ $(document).ready(function(){
 		},
 	}).data('gridster');
 
+	//gridster.add_widget('<li class="task-card" id="card03">YEAAAH</li>"', 1, 1, 3, 4)'"')
 
 	$("#svg1").attr("height", "0");
 	$("#svg1").attr("width", "0");
 	connectAll();
 
+	document.addEventListener("click", (e) => {
+		const flyoutElements = document.getElementsByClassName("task-card");
+		const flyoutArray = Array.from(flyoutElements);
+		let targetElement = e.target; // clicked element
+
+		do {
+			if (flyoutArray.includes(targetElement)) {
+				// This is a click inside. Do nothing, just return.
+				console.log("Clicked inside!");
+				return;
+			}
+				// Go up the DOM
+				targetElement = targetElement.parentNode;
+			} while (targetElement);
+
+		// This is a click outside.
+		console.log("Clicked outside!");
+	});
 })
